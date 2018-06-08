ca46f74e81fbef85ec6e915d8940bc76cbe5825d
Thiago Rodrigues <thiagorodriguescamara@gmail.com>
2018-06-07 21:33:34 -0300

    Alterado firebase.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAihc-Qj0GKRp5EGPw2H9ZXwJd_c5mRAhY",
+    authDomain: "comandavirtual-15db8.firebaseapp.com",
+    databaseURL: "https://comandavirtual-15db8.firebaseio.com",
+    projectId: "comandavirtual-15db8",
+    storageBucket: "comandavirtual-15db8.appspot.com",
+    messagingSenderId: "376661572209"
+  }
 };
